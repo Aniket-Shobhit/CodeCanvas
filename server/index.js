e226f40615fa2d54ee5ad37917b75d71139c1ddb
Aniket-Shobhit <shobhitaniket3@gmail.com>
2023-11-14 23:34:05 +0530

    minor fixes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
@@ -10,9 +10,7 @@ const PORT = 8000;
 app.use(cors());
 app.use(express.json());
 
-
-app.post('/compile', async (req,res) => {
-    
+app.post("/compile", async (req, res) => {
     let code = req.body.code;
     let language = req.body.language;
     let input = req.body.input;
@@ -21,29 +19,32 @@ app.post('/compile', async (req,res) => {
     const API_KEY = process.env.API_KEY;
     const API_HOST = process.env.API_HOST;
 
-    let data = ({
+    let data = {
         language,
         code,
         input,
-        version: 'latest',
-    });
+        version: "latest",
+    };
 
     const headers = {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': API_HOST,
-    }
+        "content-type": "application/json",
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": API_HOST,
+    };
 
     try {
         const response = await axios.post(API_URL, data, {
-            headers: headers
+            headers: headers,
         });
         res.send(response.data);
-    } catch(e) {
-        res.status(500).json({ error: 'An error occurred' });
+    } catch (e) {
+        res.status(500).json({ error: "An error occurred" });
     }
+});
 
-})
+app.get("/", (req, res) => {
+    res.send("Hello there!");
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
